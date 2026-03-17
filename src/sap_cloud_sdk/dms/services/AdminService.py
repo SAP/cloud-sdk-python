9a0723b234fc382b4ff87d8d11007f7ae7d11721
Jagnath Reddy <jagnath.reddy@sap.com>
2026-03-18 00:42:34 +0530

    adding union import

--- src/sap_cloud_sdk/dms/services/AdminService.py
+++ src/sap_cloud_sdk/dms/services/AdminService.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List,Optional
+from typing import Any, Dict, List, Optional, Union
 
 from sap_cloud_sdk.dms.model.dms_credentials import DMSCredentials
 from sap_cloud_sdk.dms.model.repository import Repository

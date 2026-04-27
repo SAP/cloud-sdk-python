ac4ae5d0ce8e08bbfca8caa36eb2ae9851fe6377
Lucas Soares <lucas.soares@sap.com>
2026-04-27 16:25:00 -0700

    Middleware base class

--- src/sap_cloud_sdk/core/telemetry/middlewares/__init__.py
+++ src/sap_cloud_sdk/core/telemetry/middlewares/__init__.py
@@ -1,4 +1,4 @@
-from sap_cloud_sdk.core.telemetry.middlewares.base import HeaderSpanMiddleware
+from sap_cloud_sdk.core.telemetry.middlewares.base import TelemetryMiddleware
 from sap_cloud_sdk.core.telemetry.middlewares.starlette import A2AStarletteMiddleware
 
-__all__ = ["HeaderSpanMiddleware", "A2AStarletteMiddleware"]
+__all__ = ["TelemetryMiddleware", "A2AStarletteMiddleware"]

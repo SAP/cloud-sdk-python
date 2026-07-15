8c7398f1f3c86f54d5de83513d9bcce4902aea70
Betina Benaduce <betina.benaduce@sap.com>
2026-07-15 09:15:31 -0300

    refactor: add new sr to destination

--- src/sap_cloud_sdk/destination/config.py
+++ src/sap_cloud_sdk/destination/config.py
@@ -27,7 +27,7 @@ from dataclasses import dataclass
 from typing import Optional
 import os
 
-from sap_cloud_sdk.core.secret_resolver.sdk_config import get_resolver
+from sap_cloud_sdk.core.secret_resolver import get_resolver
 from sap_cloud_sdk.destination.exceptions import ConfigError
 from sap_cloud_sdk.destination._models import TransparentProxy
 

61e4bc44d6ce8ab70bd2eeebd900f1b4a37dca8e
Lucas Soares <lucas.soares@sap.com>
2026-07-20 16:16:20 +0200

    als tenant inference

--- src/sap_cloud_sdk/core/telemetry/middleware/starlette_a2a.py
+++ src/sap_cloud_sdk/core/telemetry/middleware/starlette_a2a.py
@@ -32,8 +32,9 @@ class _IASMiddleware(BaseHTTPMiddleware):
         self._attrs_var = attrs_var
 
     async def dispatch(self, request: Request, call_next: Any) -> Response:
-        set_auth_context(_parse_ias_claims(request))
-        token = self._attrs_var.set(_extract_ias_attrs(request))
+        claims, attrs = _parse_request(request)
+        set_auth_context(claims)
+        token = self._attrs_var.set(attrs)
         try:
             return await call_next(request)
         finally:
@@ -81,35 +82,22 @@ class StarletteIASTelemetryMiddleware(TelemetryMiddleware):
         return self._attrs_var.get()
 
 
-def _parse_ias_claims(request: Request):
-    """Parse the Authorization header and return IASClaims, or None on failure."""
+def _parse_request(request: Request):
+    """Parse the Authorization header and return (IASClaims, telemetry_attrs)."""
     auth = request.headers.get("authorization", "")
-    if not auth:
-        return None
-    try:
-        return parse_token(auth)
-    except Exception as e:
-        logger.debug("IAS token parsing failed, skipping telemetry attrs: %s", e)
-        return None
-
-
-def _extract_ias_attrs(request: Request) -> Dict[str, Any]:
-    """Parse the Authorization header and return telemetry attributes."""
-    auth = request.headers.get("authorization", "")
-    if not auth:
-        return {}
-    try:
-        claims = parse_token(auth)
-    except Exception as e:
-        logger.debug("IAS token parsing failed, skipping telemetry attrs: %s", e)
-        return {}
-
+    claims = None
     attrs: Dict[str, Any] = {}
-    if claims.sap_gtid:
-        attrs[ATTR_SAP_TENANT_ID] = claims.sap_gtid
-    if claims.user_uuid:
-        attrs[ATTR_USER_ID] = claims.user_uuid
+    if auth:
+        try:
+            claims = parse_token(auth)
+        except Exception as e:
+            logger.debug("IAS token parsing failed, skipping telemetry attrs: %s", e)
+        if claims is not None:
+            if claims.sap_gtid:
+                attrs[ATTR_SAP_TENANT_ID] = claims.sap_gtid
+            if claims.user_uuid:
+                attrs[ATTR_USER_ID] = claims.user_uuid
     origin = request.headers.get("x-sap-origin")
     if origin:
         attrs[ATTR_SAP_TRIGGER_TYPE] = origin
-    return attrs
+    return claims, attrs
